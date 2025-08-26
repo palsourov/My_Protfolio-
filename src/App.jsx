bf366aafc54bb73d3132edb7235ac126da6e4307
palsourov <palsourov365@gmail.com>
2025-08-26 14:02:26 +0600

    refactor: restructure App component and add page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
- 
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="font-sans bg-gray-100 text-gray-800">
+      <Navbar />
+      <main className="max-w-6xl mx-auto px-4">
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
