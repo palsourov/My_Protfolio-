08d9727101f78675ad5ea4c16fd2db5be3ed9ccb
palsourov <palsourov365@gmail.com>
2026-09-17 19:37:37 +0600

    Refactor Roots component for improved readability and formatting

--- src/Pages/Root/Roots.jsx
+++ src/Pages/Root/Roots.jsx
@@ -1,17 +1,16 @@
-import React from "react";
 
+import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../../Components/Navbar/Navbar";
-import Footer from "../../Components/Footer/Footer";  
-import { div } from "framer-motion/client";
+import Footer from "../../Components/Footer/Footer";
 
 const Roots = () => {
   return (
     <div className="font-sans bg-[#051a35] text-gray-800">
-      <div className="   ">
-        <Navbar></Navbar>
-         <Outlet></Outlet>
-        <Footer></Footer>
+      <div>
+        <Navbar />
+        <Outlet />
+        <Footer />
       </div>
     </div>
   );
